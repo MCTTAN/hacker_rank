d57d347b9e5daeba68274d4eb897623eeda9f50a
Marie Chantal Tan <mariechantaltan@gmail.com>
2016-07-04 12:51:29 -0700

    Create simple_array_sum.cpp

---
+++ simple_array_sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int size, sum, i, j;
+  
+  cin >> size;
+  
+  vector<int> numbers;
+  
+  for(i = 0; i < size; ++i)
+  {
+    int value;
+    
+    cin >> value;
+    
+    numbers.push_back(value);
+  }
+  
+  for(j = 0; j < size; ++j)
+  {
+    sum += numbers.at(j);
+  }
+  
+  cout << sum;
+  
+  return 0;
+}
