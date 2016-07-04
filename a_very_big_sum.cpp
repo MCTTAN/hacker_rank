8c02789914325bd675bd06b9efda889ab26c7aa0
Marie Chantal Tan <mariechantaltan@gmail.com>
2016-07-04 12:54:29 -0700

    Update a_very_big_sum.cpp

--- a_very_big_sum.cpp
+++ a_very_big_sum.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-int main()
-{
+int main(){
   vector<long long int> numbers;
   long long int size, sum, i, j;
-  
   cin >> size;
-  
-  for(i = 0; i < size; ++i)
-  {
+  for(i = 0; i < size; ++i){
     long long int value;
-    
     cin >> value;
-    
     numbers.push_back(value);
   }
-  
   for(j = 0; j < size; ++j)
-  {
     sum += numbers.at(j);
-  }
-  
   cout << sum;
-  
   return 0;
 }
